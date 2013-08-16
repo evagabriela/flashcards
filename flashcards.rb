95a10966eab8f3d245c9ad8aceef6b0ffa9fed45
nathanallen <thenathanator@googlemail.com>
2013-08-16 16:41:45 -0700

    Final Commit

--- flashcards.rb
+++ flashcards.rb
@@ -1,3 +1,4 @@
+#require 'pry'
 class Deck
 
   def initialize
@@ -25,42 +26,43 @@ end
 
 
 class Controller
- 
-  def input_and_parse_file(filename) #File name MUST BE IN THE SAME DIRECTORY 
+
+  def self.input_and_parse_file(filename) #File name MUST BE IN THE SAME DIRECTORY
     filename_edit = "./" + filename
-    file_open = File.open(filename_edit, "r") 
+    file_open = File.open(filename_edit, "r")
     file_string = file_open.read
 
-    file_array = file_string.split('\n')
+    file_array = file_string.split("\n")
 
-      def_array = []
-      key_array =[]
-      n = 0
-      file_array.each_with_index do |string,index| 
-        if index == 3*n
-            def_array << string
-        elsif index == (3 * n) +1
-            key_array << string
-        end
+    def_array = []
+    key_array = []
+    n = 0
+    file_array.each_with_index do |string, index|
+      if index == 3 * n
+        def_array << string
+      elsif index == (3 * n) +1
+        key_array << string
+        n += 1
       end
-        @input_file = key_array.zip(def_array)
-      
+    end
+    @input_file = key_array.zip(def_array)
+
   end
 
 
-  def populate_deck #sends word and definition strings to Deck.add_card
+  def self.populate_deck #sends word and definition strings to Deck.add_card
     @deck_object = Deck.new
-
     @input_file.each do |string|
       @deck_object.add_card(string[0],string[1])
     end
+    @deck_object.shuffle
   end
 
   def self.control_sequence(txt_filename)
-      @input_file = []
-      input_and_parse_file(txt_filename)
-      populate_deck
-      @deck_object
+    @input_file = []
+    input_and_parse_file(txt_filename)
+    populate_deck
+    @deck_object
   end
 
 end
@@ -71,72 +73,75 @@ class GameMaster
   attr_accessor :current_word, :current_def
 
   def initialize(txt_file)
-     @TheDeck = Controller.control_sequence(txt_file)
+    #binding.pry
+    @TheDeck = Controller.control_sequence(txt_file)
   end
 
   def run_game
     #Welcome Message
     puts "Welcome to Ruby Flash Cards. "
-      sleep 0.1 
+    sleep 0.1
     puts "To play, just enter the correct term for each definition."
-      sleep 0.1 
+    sleep 0.1
     puts "Ready?  Go!"
-      sleep 0.1 
+    sleep 0.1
 
     keep_looping = true
 
     while keep_looping
       pick_a_card
-      
-      puts "Definition:"
-      puts @current_def
-        sleep 0.5
 
-      check_guess (gets.chomp.downcase!)
-        sleep 0.1
-      
+      puts "Definition:"
+      puts "-"*10
+      puts @current_def
+      puts "-"*10
+      sleep 0.5
+
+      puts "Guess:"
+      check_guess(gets.chomp.downcase)
+      sleep 0.1
+
       puts "Ready for next flashcard? y/n"
-      keep_looping = prompt_user(gets)
+      keep_looping = prompt_user(gets.chomp.downcase)
     end
 
-    puts "Thanks for playing!"
     exit_game
 
   end
 
   def pick_a_card
     current_card = @TheDeck.sample_card
-    @current_word = current_card.first
-    @current_def = current_card.last
+    @current_word = current_card.first.downcase
+    @current_def = current_card.last.downcase
   end
 
-  def check_guess (guess, current_word)
-    guess_count = 1
-    while true
-      puts "Guess ##{guess_count}:"
+  def check_guess (guess)
       if guess == @current_word
         puts "Correct!"
-        puts "You got it in #{guess} guess!"
-      elsif guess == "quit" || "exit" || "q" ||
-        break
-      else 
-        puts "Wrong answer."
+      elsif guess == "quit" #|| "exit" || "q"
+          exit_game
+      else
+        puts "Wrong answer. Guess again:"
+        check_guess(gets.chomp.downcase)
       end
-    end
   end
 
-  def prompt_user (input.downocase!)
-      true if input == "\r" || "y" || "yes"
-      false if input == "n" || "no"
-      exit_game if input == "quit" || "exit" || "q"
-      puts "Dude, type 'y' to continue:"
-      prompt_user(gets)
+  def prompt_user (input)
+    return true if input == "y" #|| "\r" || "yes"
+    return false if input == "n" #|| "no"
+    return exit_game if input == "quit" #|| "exit" || "q"
+    puts "Dude, type 'y' to continue:"
+    prompt_user(gets.chomp.downcase)
   end
 
   def exit_game
+    puts "Thanks for playing!"
+    exit
     #Controller.save
+    #break
   end
 
 end
 
-GameMaster.new('flashcards.txt')
+game = GameMaster.new('flashcard_samples.txt')
+game.run_game
